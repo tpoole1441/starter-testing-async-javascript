80c575cd9d9c524c8a8c73d06f98b20161a895e9
Tyler Poole <tyler.poole1441@gmail.com>
2024-08-27 18:11:05 -0500

    Cleanup Commit

--- tests/requests.test.js
+++ tests/requests.test.js
@@ -29,18 +29,41 @@ describe("requests.js", () => {
     ];
 
     it("should make a GET request to the appropriate URL", async () => {
-      // Write code here
-      expect(1).toBe(2);
+      jest.spyOn(axios, "get");
+
+      await index();
+
+      const expectedURL = `${BASE_URL}/constellations`;
+      expect(axios.get).toHaveBeenCalledWith(expectedURL);
+
+      jest.clearAllMocks();
     });
 
     it("should return a list of constellations with fewer than 10 stars with planets", async () => {
-      // Write code here
-      expect(1).toBe(2);
+      jest.spyOn(axios, "get");
+      axios.get.mockImplementation(() => Promise.resolve({ data }));
+
+      const response = await index();
+
+      const expected = data.slice(0, 2);
+      expect(response).toEqual(expected);
+
+      jest.clearAllMocks();
     });
 
     it("should log an error to the console", async () => {
-      // Write code here
-      expect(1).toBe(2);
+      jest.spyOn(axios, "get");
+      axios.get.mockImplementation(() =>
+        Promise.reject(new Error("Request failed."))
+      );
+
+      jest.spyOn(console, "error");
+
+      await index();
+
+      expect(console.error).toHaveBeenCalledWith("Request failed.");
+
+      jest.clearAllMocks();
     });
   });
 });
